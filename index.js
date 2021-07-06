bce5ce5badf765382b4c33344f5348cb7d951d8e
shuozibx <87013365+shuozibx@users.noreply.github.com>
2021-07-06 18:18:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var log = console.log.bind(console);
+
+var imgPath1 = '/img/selfie-monkey11.jpg';
+var imgPath2 = 'img/bjyx.jpg';
+var imgPath3 = 'img/ddd.jpg';
+var imgPath4 = 'img/ggg.jpg';
+var imgPath5 = 'img/selfie-monkey.jpg';
+var imgPath1 = 'img/selfie-monkey11.jpg';
+
+var cubetwo = new window.CubeTwo({
+	cubeComponent: document.getElementById('cubetwo-component-1'),
+	backgroundImages: { /* optional */
+		u: imgPath1,
+		d: imgPath2,
+		f: imgPath3,
+		b: imgPath4,
+		l: imgPath5,
+		r: imgPath1,
+	},
+	backgroundColors: { /* optional */
+		u: 'rgba(255, 255, 255, .7)',
+		f: 'rgba(0, 158, 96, .7)',
+		r: 'rgba(196, 30, 58, .7)',
+		l: 'rgba(255, 88, 0, .7)',
+		b: 'rgba(0, 81, 186, .7)',
+		d: 'rgba(255, 213, 0, .7)',
+		_: 'transparent',
+	},
+});
+
+cubetwo.init();
+
+window.addEventListener('keydown', cubetwo.handleGlobalKeyEvent, false);
+
+window.cubetwo = cubetwo;
+
+log('cubetwo is available in console');
+
+// Custom
+setTimeout(function() {
+
+	cubetwo._setState(JSON.parse(
+		'{"codes":[{"cube":6,"code":"ru"},{"cube":5,"code":"lu"},{"cube":1,"code":"bu"},{"cube":2,"code":"bu"},{"cube":4,"code":"dl"},{"cube":3,"code":"dr"},{"cube":7,"code":"fr"},{"cube":8,"code":"fl"}],"isRotateEnabled":true}'
+	));
+	cubetwo._updateUiFaces();
+
+}, 0);
